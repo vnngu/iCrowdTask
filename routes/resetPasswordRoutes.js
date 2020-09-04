e477d356271e5261d524b9dd4d62da60249a8b33
Nam Nguyen <nguyenvietnam2401@gmail.com>
2020-09-04 18:43:21 +1000

    📦 FINISHED: Reset password

--- routes/resetPasswordRoutes.js
+++ routes/resetPasswordRoutes.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const resetPasswordController = require("../controllers/resetPasswordController");
+const { isUserNotAuthenticated } = require("../functions/checkAuthentication");
 
-router.get("/forgot", resetPasswordController.get_email);
-router.post("/forgot", resetPasswordController.post_email);
+router.get(
+  "/forgot",
+  isUserNotAuthenticated,
+  resetPasswordController.get_email
+);
+router.post(
+  "/forgot",
+  isUserNotAuthenticated,
+  resetPasswordController.post_email
+);
 router.get(
   "/reset-password/:token",
+  isUserNotAuthenticated
   resetPasswordController.get_reset_password
 );
 router.post(
   "/reset-password/:token",
+  isUserNotAuthenticated
   resetPasswordController.post_reset_password
 );
 
